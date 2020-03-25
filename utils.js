d80ac59d0d7f146051f428e712387fe37db578ec
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-03-25 14:57:32 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+// Check if window has closed
 function detectClose(w) {
     var timer = setInterval(function () {
             if (w.closed) {
@@ -8,17 +9,17 @@ function detectClose(w) {
 }
 
 function defence() {
+    // Prevent Context menu
     document.addEventListener('contextmenu', event => event.preventDefault());
-    document.onkeydown = function(e) {
-        return false
-    };
+    // Prevent shortcuts
+    document.addEventListener('keypress', event => event.preventDefault());
+    // Detect DevTools
     var done = false;
     var element = new Image();
-	
     Object.defineProperty(element, 'id', {
         get: function () {
             if (done) return
-			done = true;
+            done = true;
             trap();
         }
     });
