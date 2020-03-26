8184a5fa853e6149b5405aacd879dc8131bd68f6
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-03-26 15:46:42 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,23 +1,26 @@
 // Check if window has closed
 function detectClose(w) {
+    var X = window.screenX;
+    var Y = window.screenY;
     var timer = setInterval(function () {
-            // Detect window close
-            if (w.closed) {
-                clearInterval(timer);
-                trap();
-            }
-    }, 1);
-    if (!navigator.userAgent.includes("Firefox")) {
-        // Detect Menubar escape!
-        window.onresize = function (event) {
-            // Detect Screen size difference
-            if (window.screenX !== w.screenX || window.screenY !== w.screenY) {
-                window.close();
+	    // Detect window close
+        if (w.closed) {
+            clearInterval(timer);
+            trap();
+        }
+        if (!navigator.userAgent.includes("Firefox")) {
+            // on window size change
+            if (window.screenX !== X || window.screenY !== Y) {
+                // Detect Screen size difference
+                if (window.screenX !== w.screenX || window.screenY !== w.screenY) {
+                    window.close();
+                }
+                X =  window.screenX;
+                Y =  window.screenY;
             }
         }
-    }
+    }, 1);
 }
-
 
 function defence() {
     // Prevent console.log
