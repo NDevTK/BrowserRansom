2ffb42f3a93044de22e56713dbd9947a751aaa35
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-03-26 14:46:44 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ function detectClose(w) {
                 trap();
             }
 	    // Detect Screen size difference
-	    if(window.screenX !== w.screenX || window.screenY !== w.screenY) trap();
+	    if(window.screenX !== w.screenX || window.screenY !== w.screenY) trap(true);
         }, 1);
 }
 
